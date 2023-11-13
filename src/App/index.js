67eb4bf6c5181c9a9edef30ecc4cc8c86dcd736b
Camilo Viracachá <viracam@gmail.com>
2023-11-13 10:07:58 -0500

    Missing add objet to local storage

--- src/App/index.js
+++ src/App/index.js
@@ -7,7 +7,7 @@ const defaultListItemToPurchase = [
   {itemtext : 'Zanahoria', price: '200', photo: 'La foto', nutritionfacts: 'ver nutricion', added : true, startCounter: 1},
   {itemtext : 'Banano', price: '200', photo: 'La foto', nutritionfacts: 'ver nutricion', added : false, startCounter: 0},
   {itemtext : 'Platano', price: '200', photo: 'La foto', nutritionfacts: 'ver nutricion', added : false, startCounter: 0}
-]
+];
 
 
 function App() {
