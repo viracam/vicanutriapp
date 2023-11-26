1dde32c418933d2fae5285fd42ec90ed063694cd
Camilo Viracachá <viracam@gmail.com>
2023-11-26 11:14:34 -0500

    review toggle botton and added function

--- src/ListForm/index.js
+++ src/ListForm/index.js
@@ -2,21 +2,34 @@ import React from "react";
 import { ListContext } from "../ListContext";
 
 function ListFrom(){
+    const [newListValue, setNewListValue] = React.useState()
+
     const{
+        added,
 
     } = React.useContext(ListContext)
 
+    const onChange = (event) =>{
+        setNewListValue(event.target.value)
+
+    }
     const onCancel = () =>{
 
     }
-    const onSubmit = () =>{
+    const onSubmit = (event) =>{
+        event.preventDefault();
+        added(newListValue);
         
     }
 
     return(
         <form onSubmit={onSubmit}>
             <label>Añade alimento</label>
-            <input placeholder="Pera, cebolla, etc..."></input>
+            <input 
+                placeholder="Pera, cebolla, etc..."
+                onChange={onChange}
+                value={newListValue}
+            ></input>
             <label>Añade el valor</label>
             <input placeholder="$200 o $300"></input>
             <div>
